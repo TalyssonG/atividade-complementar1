d638962ad6e4b66d141de5cda3ba51d3dfb7ceab
TalyssonG <lopestalysson99@gmail.com>
2024-08-27 19:24:09 -0300

    modificação na condiçâo operação

--- app.js
+++ app.js
@@ -7,37 +7,33 @@ app.get('/calculadora', (req, res) => {
     const n1 = parseFloat(req.query.n1);
     const n2 = parseFloat(req.query.n2);
 
-    //---------------- Verificação dos parâmetros obrigatórios
+    // ------------ verificação
     if (!operacao || isNaN(n1) || isNaN(n2)) {
-        return res.status(400).json({ error: 'Bad request. Please provide valid "operacao", "n1", and "n2" query parameters.' });
-    }
-    let resultado;
-
-    //------------------- Realização da operação solicitada
-    switch (operacao) {
-        case 'soma':
-            resultado = n1 + n2;
-            break;
-        case 'subtracao':
-            resultado = n1 - n2;
-            break;
-        case 'multiplicacao':
-            resultado = n1 * n2;
-            break;
-        case 'divisao':
-            if (n2 === 0) {
-                return res.status(400).json({ error: 'Bad request. Division by zero is not allowed.' });
-            }
-            resultado = n1 / n2;
-            break;
-        default:
-            return res.status(400).json({ error: 'Bad request. Invalid "operacao" parameter. Use "soma", "subtracao", "multiplicacao", or "divisao".' });
+        return res.status(400).json({ error: 'Parâmetros inválidos.' });
     }
 
-    // --------------------- Retorno do resultado da operação
-    res.json({ resultado: resultado });
+    // ------------ Funções para as operações - Objeto 
+    const operacoes = {
+        'soma': (a, b) => a + b,
+        'subtracao': (a, b) => a - b,
+        'multiplicacao': (a, b) => a * b,
+        'divisao': (a, b) => b !== 0 ? a / b : null,
+    };
+
+    const operacaoFuncao = operacoes[operacao];
+    if (!operacaoFuncao) {
+        return res.status(400).json({ error: 'Operação inválida.' });
+    }
+
+    const resultado = operacaoFuncao(n1, n2);
+    if (resultado === null) {
+        return res.status(400).json({ error: 'Divisão por zero.' });
+    }
+
+    // Retorno do resultado
+    res.json({ resultado });
 });
 
 app.listen(port, () => {
-    console.log(`Web Service running at http://localhost:${port}`);
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
